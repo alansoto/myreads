9823ca6d7218fe538b9717068c92f00aab56d069
alan <me@alansoto.info>
2017-10-03 21:37:54 +1000

    refactor: add describe section to book component tests

--- src/Book/Book.test.js
+++ src/Book/Book.test.js
@@ -5,27 +5,30 @@ import Book from './Book';
 
 Enzyme.configure({adapter: new Adapter()});
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<Book/>);
-  expect(wrapper).toMatchSnapshot();
-});
+describe('Book Component',()=>{
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Book/>);
+    expect(wrapper).toMatchSnapshot();
+  });
 
-it('renders backgroundCover coming from props',()=>{
-  const wrapper = shallow(<Book backgroundCover='http'/>)
-  expect(wrapper.find('.book-cover').get(0).props.style.backgroundImage).toBe('url(http)')
-})
+  it('renders backgroundCover coming from props',()=>{
+    const wrapper = shallow(<Book backgroundCover='http'/>)
+    expect(wrapper.find('.book-cover').get(0).props.style.backgroundImage).toBe('url(http)')
+  })
 
-it('renders bookTitle coming from props',()=>{
-  const wrapper = shallow(<Book bookTitle='Hamlet'/>)
-  expect(wrapper.find('.book-title').text()).toBe('Hamlet')
-})
+  it('renders bookTitle coming from props',()=>{
+    const wrapper = shallow(<Book bookTitle='Hamlet'/>)
+    expect(wrapper.find('.book-title').text()).toBe('Hamlet')
+  })
 
-it('renders bookAuthors coming from props',()=>{
-  const wrapper = shallow(<Book bookAuthors='Shakespeare'/>)
-  expect(wrapper.find('.book-authors').text()).toBe('Shakespeare')
-})
+  it('renders bookAuthors coming from props',()=>{
+    const wrapper = shallow(<Book bookAuthors='Shakespeare'/>)
+    expect(wrapper.find('.book-authors').text()).toBe('Shakespeare')
+  })
 
-it('renders currentBookshelf coming from props',()=>{
-  const wrapper = shallow(<Book bookshelf='test-bookshelf'/>)
-  expect(wrapper.find('.book-shelf-changer select').get(0).props.value).toBe('test-bookshelf')
+  it('renders currentBookshelf coming from props',()=>{
+    const wrapper = shallow(<Book bookshelf='test-bookshelf'/>)
+    expect(wrapper.find('.book-shelf-changer select').get(0).props.value).toBe('test-bookshelf')
+  })
+
 })
