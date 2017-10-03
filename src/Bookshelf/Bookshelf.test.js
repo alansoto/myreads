f1535f8bd8632b4efc09a6b130d0f800d362b0fd
alan <me@alansoto.info>
2017-10-03 21:33:51 +1000

    test: add smoke test for Bookshelf component

---
+++ src/Bookshelf/Bookshelf.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import Bookshelf from './Bookshelf';
+
+Enzyme.configure({adapter: new Adapter()});
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<Bookshelf/>);
+  expect(wrapper).toMatchSnapshot();
+});
